e1fbe7dfc8b8c13f4ef72b14467d70874fc54148
Emerson Garrido <emersongarrido@hotmail.com.br>
2025-03-27 00:17:40 -0300

    teste

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,35 @@
-import type React from "react"
-import type { Metadata } from "next"
-import "./globals.css"
+import type React from "react";
+import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "v0 App",
-  description: "Created with v0",
-  generator: "v0.dev",
-}
+  title: "Colheita Infeliz ",
+  description: "Versao Brasil",
+  generator: "emersongarrido.dev",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+        />
         <meta name="theme-color" content="#22c55e" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
       </head>
       <body>{children}</body>
     </html>
-  )
+  );
 }
 
-
-
-import './globals.css'
+import "./globals.css";
